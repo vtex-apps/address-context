842dbee79951ffe12b75c163c5a6de1a38a28447
Maxi <maximiliano.redionda@vtex.com.br>
2020-01-16 13:58:45 -0300

    Initial ochestrator configuration

---
+++ react/AddressContext.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, ReactNode, useContext } from 'react'
+
+interface Context {
+  countries: String[]
+}
+
+interface AddressContextProps {
+  children: ReactNode
+}
+
+const AddressContextContext = createContext<Context | undefined>(undefined)
+
+export const AddressContextProvider = ({ children }: AddressContextProps) => {
+  const ok = { countries: ['BRA', 'ARG'] }
+
+  return (
+    <AddressContextContext.Provider value={{ ok }}>
+      {children}
+    </AddressContextContext.Provider>
+  )
+}
+
+export const useAddressContext = () => {
+  const context = useContext(AddressContextContext)
+  if (context == undefined) {
+    throw new Error(
+      'useAddressContext must be used within an AddressContextProvider'
+    )
+  }
+
+  return context
+}
+
+export default { AddressContextProvider, useAddressContext }
