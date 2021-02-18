7fb6ea5cb4547832749b6b24a54eb8ab015c7be3
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-02-18 14:06:08 -0300

    Add strong types to address rules hook return value

--- react/useAddressRules.tsx
+++ react/useAddressRules.tsx
@@ -1,15 +1,34 @@
 import { useMemo } from 'react'
 import { useQuery } from 'react-apollo'
+import {
+  CountryData,
+  DisplayData,
+  AddressFieldsData,
+  LocationSelectData,
+} from 'vtex.country-data-settings'
 
 import addressRulesQuery from './graphql/addressRulesQuery.gql'
 
+interface RulesQuery {
+  allCountriesData: CountryData[]
+}
+
 const useAddressRules = () => {
-  const { data } = useQuery(addressRulesQuery)
+  const { data } = useQuery<RulesQuery>(addressRulesQuery)
 
   const addressRules = useMemo(
     () =>
-      data?.allCountriesData.reduce(
-        (rules: any, countryData: any) => ({
+      data?.allCountriesData?.reduce<
+        Record<
+          string,
+          {
+            display: DisplayData
+            fields: AddressFieldsData
+            locationSelect?: LocationSelectData | null
+          }
+        >
+      >(
+        (rules, countryData) => ({
           ...rules,
           [countryData.countryISO]: {
             display: countryData.display,
